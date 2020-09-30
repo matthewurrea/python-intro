a8984dd44b9d6cad2d0987d04b9014aa0796456f
Matthew Urrea <matt.urrea.code@gmail.com>
2020-09-30 07:23:00 -0600

    continued python practice

--- functions.py
+++ functions.py
@@ -2,8 +2,10 @@
 # Collection of code that performs a specific task. 
 
 # function that says hi
-def say_hi(): 
-     print("Hello User")
+def say_hi(name, age): 
+     print("Hello " + name + ", you are" + str(age))
 
 # Calling the function. 
-say_hi()
+say_hi("Mike", 35)
+say_hi("Steve", 70)
+
---
+++ return-statement.py
@@ -0,0 +1,8 @@
+# Using the Return Statement in Functions
+
+# Function that cubes a number. 
+def cube(num):
+    num*num*num
+
+cube(3)
+
